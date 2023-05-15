b5fd90286e77ac87c338984a4ad360e1e6e368bf
cw27 <kimcw001227@gmail.com>
2023-05-15 10:58:53 +0900

    kg pay

--- src/kg_pay/KgService.java
+++ src/kg_pay/KgService.java
@@ -1,5 +1,7 @@
 package kg_pay;
 
 public interface KgService {
-
+	public boolean correct(String cNum, String cvc, String cPwd);
+	public int k_charge(int pay);
+	public int k_point(int point1);
 }
--- src/kg_pay/Kg_Service.java
+++ src/kg_pay/Kg_Service.java
@@ -1,17 +1,5 @@
 package kg_pay;
 
 public class Kg_Service {
-	public boolean tf1 = true;
-	public int chargePay = 0;
-	public int point1 = 0;
-	
-	public boolean correct(String cNum, String cvc, String cPwd) {
-		return tf1;
-	}
-	public int k_charge(int pay) {
-		return chargePay;
-	}
-	public int k_point(int point1) {
-		return point1;
-	}
+
 }
