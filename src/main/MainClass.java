01488a05da2872dfdb60e108e267b5bf5c565382
im222b <rudgnsdleh@naver.com>
2023-05-17 10:38:36 +0900

    생성 완료

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,18 @@
 package main;
 
+import java.util.Scanner;
+
+import common.DBConnection;
+import company.Product;
+import kg_pay.regpay;
+import kgpayDTO.KgpayDTO;
+
 public class MainClass {
+	public static void main(String[] args) {
+		Product pr = new Product();
+		regpay rg = new regpay();
+		rg.regSelect();
+		//pr.pdSelect();
+	}
 
 }
